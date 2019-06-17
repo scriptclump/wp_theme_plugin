d783d193980823ad0420c7e4f7a8e8dc9b4044db
Basant Kumar Sharma <scriptclump@gmail.com>
2019-06-17 17:59:15 +0530

    Add header, footer to theme

--- wp-content/themes/scriptclump/index.php
+++ wp-content/themes/scriptclump/index.php
@@ -1,52 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Blog Home - Start Bootstrap Template</title>
-  <?php wp_head(); ?>
-</head>
-
-<body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <?php wp_nav_menu(array(
-          'theme_location' => 'primary',
-          'container'      => false,
-          'menu_class'     => 'navbar-nav ml-auto'
-        )); ?>
-        <!-- <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul> -->
-      </div>
-    </div>
-  </nav>
-
+<?php get_header(); ?>
   <!-- Page Content -->
   <div class="container">
 
@@ -179,15 +131,4 @@
 
   </div>
   <!-- /.container -->
-
-  <!-- Footer -->
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
-
-   <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
